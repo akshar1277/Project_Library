a2975a9e17cc3659094ca5d9de7f3e094bed9730
akshar1277 <bhalani.akshar1277@gmail.com>
2023-02-16 14:17:58 +0530

    pagination css done

--- src/components/AreaWiseProjects/Project.js
+++ src/components/AreaWiseProjects/Project.js
@@ -300,22 +300,38 @@ const Project = () => {
               );
             })}
 
+         
+        </div>
+      </div>
+
+      <div  className="grid md:grid-cols-12 grid-cols-1 mt-8 mb-16">
+        <div className="md:col-span-12 text-center">
+        <nav aria-label="Page navigation example">
+        
+
+            
           <ReactPaginate
             breakLabel="..."
-            nextLabel=">"
+            breakClassName={"w-[40px] h-[40px] inline-flex justify-center items-center text-slate-400 hover:text-white bg-white dark:bg-slate-900 border border-gray-100 dark:border-gray-700 hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"}
+            nextLabel={<i class="uil uil-angle-right text-[20px] rtl:rotate-180 rtl:-mt-1"></i>}
+            
             onPageChange={handlePageClick}
             pageRangeDisplayed={5}
             pageCount={pageCount}
-            previousLabel="<"
+            previousLabel={ <i class="uil uil-angle-left text-[20px] rtl:rotate-180 rtl:-mt-1"></i>}
             renderOnZeroPageCount={null}
             containerClassName={
-              "member__pagination mt-4 member__pagination--right        default-pagination"
+              "inline-flex items-center -space-x-px"
             }
-            nextLinkClassName={"pagination__link"}
-            activeClassName={"pagination__link--active"}
+            nextLinkClassName={"w-[40px] h-[40px] inline-flex justify-center items-center text-slate-400 bg-white dark:bg-slate-900 ltr:rounded-r-lg rtl:rounded-l-lg hover:text-white border border-gray-100 dark:border-gray-700 hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"}
+            previousLinkClassName={"w-[40px] h-[40px] inline-flex justify-center items-center text-slate-400 bg-white dark:bg-slate-900 ltr:rounded-l-lg rtl:rounded-r-lg hover:text-white border border-gray-100 dark:border-gray-700 hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"}
+            activeClassName={"z-10 w-[40px] h-[40px] inline-flex justify-center items-center text-white bg-indigo-600 border border-indigo-600"}
+            pageClassName={"w-[40px] h-[40px] inline-flex justify-center items-center text-slate-400 hover:text-white bg-white dark:bg-slate-900 border border-gray-100 dark:border-gray-700 hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600"}
+           
           />
-        </div>
-      </div>
+          </nav>
+          </div>
+          </div>
     </>
   );
 };
